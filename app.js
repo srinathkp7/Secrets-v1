2a420d7f4315d4734262a9c2fd62d870a1ef382e
srinathkp7 <srinathkp7@gmail.com>
2020-08-28 19:53:28 +0530

    Add new callbackURL

--- app.js
+++ app.js
@@ -70,7 +70,7 @@ passport.deserializeUser(function(id, done) {
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "https://whispering-reaches-13931.herokuapp.com/auth/google/secrets",
+    callbackURL: "https://secrets-v1.herokuapp.com/auth/google/secrets",
     userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
   },
   function(accessToken, refreshToken, profile, cb) {
